1f418d7f5eae64a0b4028341ef0cb209331cec31
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2024-09-27 08:35:13 +0000

    Allow for user trigered dataset install
    E.g. for Docker images

--- install_dataset.py
+++ install_dataset.py
@@ -1,40 +1,40 @@
-
-echo 'curl finished; current workspace:'
-ls -lrt
-
-echo 'untar'
-tar -xvf dpd_data.tar.gz
-rm dpd_data.tar.gz
-
-echo 'untar finished, current workspace:'
-echo 'pwd' $(pwd)
-ls -lrt
-
-cd ..
-
-echo 'run python to unpack from '
-echo 'pwd=' $(pwd)
-ls -lrt
-
-python << EOF
+import requests
 import os
 from pyiron_base import Project
 import shutil
 import tarfile
+
+DPD_data_dir = 'DPD_data'
+DPD_tar_file = 'dpd_data.tar.gz'
+
+
+with requests.get('https://datashare.mpcdf.mpg.de/public.php/webdav/', auth=('8T8WHAo1NLpgAkW', ''), stram=True) as r:
+    r.raise_for_status()
+    with open(DPD_tar_file, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=8192): 
+            f.write(chunk)
+
+
+os.mkdir(DPD_data_dir)
+with tarfile.open(DPD_tar_file) as f:
+    f.extractall(DPD_data_dir)
+
+os.remove(DPD_tar_file)
+
 pr = Project('.')
 print(f'project to operate on: {pr} content: {pr.list_all()}')
-for project in os.listdir('DPD_data'):
+for project in os.listdir(DPD_data_dir):
     w_pr = Project('wdir')
     if not project.endswith('.tar.gz'):
         continue
     print(f'Working on: {project}')
-    name = project[:-12]
+    name = project[:-len('_data.tar.gz')]
     
     print(f'file: {project} content: {pr.list_all()}')
-    shutil.copy2(os.path.join('DPD_data', project), os.path.join(w_pr.path, project))
+    shutil.copy2(os.path.join(DPD_data_dir, project), os.path.join(w_pr.path, project))
     with tarfile.open(os.path.join(w_pr.path, project)) as f:
         f.extractall(w_pr.path)
-    shutil.copy2(os.path.join('DPD_data', f"{name}_export.csv"), os.path.join(w_pr.path, 'export.csv'))
+    shutil.copy2(os.path.join(DPD_data_dir, f"{name}_export.csv"), os.path.join(w_pr.path, 'export.csv'))
     
     print(f'file: {project} content after copy: {pr.list_all()}')
     pr.open(name).unpack(w_pr.path)
@@ -51,5 +51,4 @@ for node in pr_tables.list_nodes():
     if 'project' in input_hdf.list_nodes():
         del input_hdf['project']
 
-EOF
 
